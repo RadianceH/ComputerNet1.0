4bacd6360f5f3afe46f9ba7693cad72ac5aa97c3
jiachenyang <1457901550@qq.com>
2017-12-25 08:55:56 +0800

    类

--- ComputerNet1.0/ComputerNet1.0/源.cpp
+++ ComputerNet1.0/ComputerNet1.0/源.cpp
@@ -1,3 +1,22 @@
 #include<iostream>
+using namespace std;
 
+typedef struct EdgeNode
+{
+	int adjvex;
+	int weight;
+	struct EdgeNode *next;
+}EdgeNode;
+
+typedef struct VextexNode
+{
+	char date;
+	EdgeNode *firstNode;
+}vextexNode,Adjlist[100];
+
+typedef struct
+{
+	Adjlist adjlist;
+	int numNodes, numEdges;
+}GraphAdjList;
 int main(){}
