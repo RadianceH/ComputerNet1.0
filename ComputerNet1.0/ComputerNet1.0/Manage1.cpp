271ab77d6f299420c58fd1a5724b0c8396572cc4
RadianceH <34643033+RadianceH@users.noreply.github.com>
2017-12-25 17:14:02 +0800

    /。

---
+++ ComputerNet1.0/ComputerNet1.0/Manage1.cpp
@@ -0,0 +1,29 @@
+#include"Manage.h"
+#include<string>
+#include<fstream> 
+#include<iostream>
+using namespace std;
+
+
+
+Manage::Manage() {
+
+}
+
+Manage::~Manage() {
+
+}
+
+
+void Manage::print() {
+	cout << "Â·ÓÉºÅ" << endl;
+	for (int a = 0; a<numNodes; a++)
+	{
+		cout << vexs[a] << "	";
+		for (int b = 0; b<numNodes; b++)
+		{
+			cout << arc[a][b] << "	";
+		}
+		cout << endl;
+	}
+}
