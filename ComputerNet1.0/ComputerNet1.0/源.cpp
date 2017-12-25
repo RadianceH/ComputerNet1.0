62188c7d32e7b7d507632f46e61784af7da04d98
jiachenyang <1457901550@qq.com>
2017-12-25 09:09:12 +0800

    类

--- ComputerNet1.0/ComputerNet1.0/源.cpp
+++ ComputerNet1.0/ComputerNet1.0/源.cpp
@@ -1,22 +1,23 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-typedef struct EdgeNode
+typedef struct EdgeNode//边表结点
 {
-	int adjvex;
-	int weight;
-	struct EdgeNode *next;
+	int adjvex;   //存储该顶点对应的下标
+	int weight;   //用于存储权值
+	struct EdgeNode *next;  //链域，指向下一个邻接点
 }EdgeNode;
 
-typedef struct VextexNode
+typedef struct VextexNode  //顶点表结点
 {
-	char date;
-	EdgeNode *firstNode;
+	char date;   //顶点域，存储顶点信息 ip
+	EdgeNode *firstNode;//边表头指针
 }vextexNode,Adjlist[100];
 
 typedef struct
 {
 	Adjlist adjlist;
-	int numNodes, numEdges;
+	int numNodes, numEdges;//图中当前顶点数与边数
 }GraphAdjList;
 int main(){}
